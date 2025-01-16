7d2a90685ff80d8b53eb3c1b9370a346df0dcbd0
Anh Duc <aducp66@gmail.com>
2025-01-16 13:44:56 +0700

    first commit

---
+++ check-structure.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const REQUIRED_FOLDERS = [
+  'public',
+  'src',
+  'src/assets',
+  'src/components',
+  'src/pages',
+  'src/styles',
+  'src/utils',
+];
+
+const missingFolders = REQUIRED_FOLDERS.filter(folder => !fs.existsSync(folder));
+
+if (missingFolders.length > 0) {
+  console.error('Cấu trúc dự án không hợp lệ. Thiếu:
+', missingFolders.map(f => `- ${f}/`).join('\n'));
+  process.exit(1);
+} else {
+  console.log('✅ Cấu trúc dự án hợp lệ!');
+}
